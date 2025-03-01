82f3c80b140345eabb11356fed8bdce62133440d
sodiki366 <mark1704kos@gmail.com>
2025-03-01 10:25:18 +0300

    ывап

--- 1.3 исскуство.py
+++ 1.3 исскуство.py
@@ -14,7 +14,8 @@ pygame.display.set_caption("My Game")
 COLORS = [RED,GREEN, BLUE, YELLOW, CYAN, MAGENTA, GRAY, ORANGE, PINK, BROWN , PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL, SILVER, GOLD]
 BACKGROUND = (255, 255, 255)
 screen.fill(BACKGROUND)
-
+circles = []
+circle_timer = 0
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -29,6 +30,16 @@ while running:
             running = False
     # Основная логика игры
     BACKGROUND = random.choice(COLORS)
+    circle_timer += clock.get_time()
+    if circle_timer >= 1000:  # 1000 миллисекунд = 1 секунда
+        circle_timer = 0
+        num_circles = random.randint(1, 10)  # Рандомное количество кругов от 1 до 10
+        for _ in range(num_circles):
+            x = random.randint(0, 1280)
+            y = random.randint(0, 720)
+            COLOR = random.choice(COLORS)
+            radius = random.randint(10, 100)  # Минимальный радиус 10, чтобы круги были видимыми
+            circles.append((x, y, COLOR, radius))
 
     # Отрисовка объектов
     x = random.randint(0,1280)
